714b04f1fe074a68017de53da8c214a061571a48
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2022-04-23 22:24:17 -0400

    improving on naive binary tree soltuon

--- trees/basics_2.cpp
+++ trees/basics_2.cpp
@@ -14,6 +14,41 @@ public:
     this->right = NULL;
   }
 };
+// this block is for level order traversal naive approach
+int findHeightHelper(TreeNode *root)
+{
+  if (root == NULL)
+    return 0;
+  return 1 + max(findHeightHelper(root->left), findHeightHelper(root->right));
+}
+
+void printAtLevel(TreeNode *root, int level, int curr)
+{
+  if (root == NULL)
+    return;
+  if (level == curr)
+  {
+    cout << root->data << " ";
+    return;
+  }
+
+  printAtLevel(root->left, level, curr + 1);
+  printAtLevel(root->right, level, curr + 1);
+}
+
+void levelOrderTraversal(TreeNode *root)
+{
+  int heightOfTree = findHeightHelper(root);
+  int n = 1;
+  while (heightOfTree >= n)
+  {
+    cout << n << ": ";
+    printAtLevel(root, n, 1);
+    cout << endl;
+    n++;
+  }
+}
+// this block is for level order traversal using queue
 
 void printNodesAtKthLevel(TreeNode *root, int k, int curr)
 {
@@ -35,7 +70,10 @@ int main()
   root1->left->left = new TreeNode(40);
   root1->left->right = new TreeNode(50);
   root1->right->right = new TreeNode(70);
-  printNodesAtKthLevel(root1, 2, 0);
+  // printNodesAtKthLevel(root1, 2, 0);
+  cout << endl;
+  cout << "level order traversal naive method " << endl;
+  levelOrderTraversal(root1);
   cout << endl;
 
   return 0;
